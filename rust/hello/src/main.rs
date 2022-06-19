6c47c9347a37c5eb4c1a64f476020e15fdb00e93
atgane <hyper2012@naver.com>
2022-06-19 10:57:12 +0900

    Docs: rust

--- rust/hello/src/main.rs
+++ rust/hello/src/main.rs
@@ -1,39 +1,23 @@
+
+#![allow(unused)]
 fn main() {
+use std::io;
+use std::io::Read;
+use std::fs::File;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
     let mut s = String::new();
-    std::io::stdin().read_line(&mut s).expect("Failed to read line.");
-    let n = s.trim().parse::<usize>().expect("Failed to convert int.");
-    s.clear();
 
-    std::io::stdin().read_line(&mut s).unwrap();
-    let arr = s.trim().split(" ").map(|x| x.parse::<i32>().expect("failed to convert int."))
-    .collect::<Vec<i32>>();
-
-    let mut memo: Vec<i32> = Vec::new();
-
-    for i in 0..n {
-        if i == 0 {
-            memo.push(1);
-            continue;
-        }
-        let mut max_val = 0;
-        let mut max_cnt = 0;
-        for j in 0..i {
-            if max_cnt < memo[j] && arr[j] < arr[i] {
-                max_val = arr[j];
-                max_cnt = memo[j];
-            }
-            else if max_cnt == memo[j] && arr[j] < max_val {
-                max_val = arr[j];
-            }
-        }
-        memo.push(max_cnt + 1);
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
     }
-
-    let mut ans: usize = 0;
-    for i in 0..n {
-        if memo[ans] < memo[i] {
-            ans = i;
-        }
-    }
-    println!("{}", memo[ans]);
+}
 }
